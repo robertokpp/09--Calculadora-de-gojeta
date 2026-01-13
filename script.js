5bf158553c29a84a101e4b34ef2046954db3cef3
Roberto Kennedy <roberto.kpp@gmail.com>
2026-01-12 23:20:18 -0500

    implementa a lógica da calculadora de gorjeta e ajusta o estilo do botão

---
+++ script.js
@@ -0,0 +1,36 @@
+const vlr = document.querySelector("#vlr");
+const select = document.querySelector("select");
+
+const btn = document.querySelector("#btn");
+
+const outputVlrG = document.querySelector("#vlrG");
+const outputVlrT = document.querySelector("#vlrT");
+
+btn.addEventListener("click", calc);
+
+vlr.addEventListener("input", ()=>{
+  vlr.value.replace(/\D+/g, '')
+})
+
+
+function calc() {
+
+  let selecValue = select.value;
+  const vlrValue = Number(vlr.value)
+  let vlrGorjeta;
+
+  if (selecValue === "excelente") {
+    vlrGorjeta = (10 * vlrValue) / 100;
+  } else if (selecValue === "otimo") {
+    vlrGorjeta = (8 * vlrValue) / 100;
+  } else if (selecValue === "bom") {
+    vlrGorjeta = (5 * vlrValue) / 100;
+  } else {
+    vlrGorjeta = (2 * vlrValue) / 100;
+  }
+
+  outputVlrG.value = vlrGorjeta;
+  let soma = (vlrValue + vlrGorjeta);
+  outputVlrT.value = soma
+  console.log(vlrGorjeta);
+}

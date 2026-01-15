038fd1b049d66aba71d2d01300d07f96823671f4
Roberto Kennedy <roberto.kpp@gmail.com>
2026-01-15 10:51:58 -0500

    atualiza a lógica de cálculo da gorjeta e ajusta os valores das opções de serviço

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
-const vlr = document.querySelector("#vlr");
-const select = document.querySelector("select");
-
 const btn = document.querySelector("#btn");
 
 const outputVlrG = document.querySelector("#vlrG");
 const outputVlrT = document.querySelector("#vlrT");
+
+function calc() {
+  const vlr = Number(document.querySelector("#vlr").value);
+  const select = Number(document.querySelector("select").value);
+
+  const soma = (select * vlr) / 100;
+
+  console.log(soma)
+  outputVlrG.value = soma
+  outputVlrT.value = soma + vlr
+}
 
 btn.addEventListener("click", calc);
 
 vlr.addEventListener("input", () => {
   vlr.value = vlr.value.replace(/\D+/g, "");
 });
-
-function calc() {
-  let selecValue = select.value;
-  const vlrValue = Number(vlr.value);
-  let vlrGorjeta;
-
-  if (selecValue === "excelente") {
-    vlrGorjeta = (10 * vlrValue) / 100;
-  } else if (selecValue === "otimo") {
-    vlrGorjeta = (8 * vlrValue) / 100;
-  } else if (selecValue === "bom") {
-    vlrGorjeta = (5 * vlrValue) / 100;
-  } else {
-    vlrGorjeta = (2 * vlrValue) / 100;
-  }
-
-  outputVlrG.value = `R$ ${vlrGorjeta} `;
-  let soma = vlrValue + vlrGorjeta;
-  outputVlrT.value = `R$ ${soma}`;
-}
